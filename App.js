cd716557d698e99ac09cafa0db26f395e85c40f1
Júlia Penz <juliapenz_@hotmail.com>
2024-06-04 21:42:35 +0100

    resolvendo a dimensão do logo

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
-import {View, Text, StyleSheet, Image} from 'react-native'
+import React from 'react';
+import {View, Text, StyleSheet, Image, Dimensions} from 'react-native'
 
 export default function App(){
   return (
     <View style = {StyleSheet.container}>
       <Image source={require('./src/assets/logo.png')}
         style={styles.logo} 
-        resizeMode='contain'
+        resizeMode='center'
       ></Image>
 
       <Text style= {styles.title}>PetAlerta</Text>
     </View>
   )
 }
+const { width, height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   container:{
@@ -21,7 +23,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   logo:{
-    widht: widht,
-    height: height,
+    width: width * 0.8,
+    height: height * 0.4,
   }
 })
